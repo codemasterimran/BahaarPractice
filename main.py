24a6fd44aafb3ad94a1b6720b1f66dcf897c1645
Suraj <93871430+SunX97@users.noreply.github.com>
2024-04-30 23:50:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import numpy as np
 
 #Tensorflow Model Prediction
 def model_prediction(test_image):
-    model = tf.keras.models.load_model("trained_plant_disease_model.keras")
-    image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
-    input_arr = tf.keras.preprocessing.image.img_to_array(image)
+    model = tensorflow.keras.models.load_model("trained_plant_disease_model.keras")
+    image = tensorflow.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
+    input_arr = tensorflow.keras.preprocessing.image.img_to_array(image)
     input_arr = np.array([input_arr]) #convert single image to batch
     predictions = model.predict(input_arr)
     return np.argmax(predictions) #return index of max element
